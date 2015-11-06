3d241d5fc674321ccde1e9032799300ee9db64b7
LeaMiage <lea.cheoux@gmail.com>
2015-11-06 17:31:57 +0100

    recevoir et poster

--- sql-lea.java
+++ sql-lea.java
@@ -20,3 +20,23 @@ try {
 			" idUSuivi int FOREIGN KEY REFERENCES UTILISATEUR(idU))" + 
 			"CONSTRAINT pk_est_abonne PRIMARY KEY (idUSuiveur, idUSuivi)");
 }
+try {
+	s.executeQuery(query);
+} catch(Exception e) {
+	// sinon on l'a cree
+	s.execute("DROP TABLE POSTER");
+	s.execute("create table POSTER  ( " +
+			" idM int FOREIGN KEY REFERENCES MESSAGES(idM), " +
+			" idU int FOREIGN KEY REFERENCES UTILISATEUR(idU))" + 
+			"CONSTRAINT pk_poster PRIMARY KEY (idM, idU)");
+}
+try {
+	s.executeQuery(query);
+} catch(Exception e) {
+	// sinon on l'a cree
+	s.execute("DROP TABLE RECEVOIR");
+	s.execute("create table RECEVOIR  ( " +
+			" idM int FOREIGN KEY REFERENCES MESSAGES(idM), " +
+			" idU int FOREIGN KEY REFERENCES UTILISATEUR(idU))" + 
+			"CONSTRAINT pk_poster PRIMARY KEY (idM, idU)");
+}
