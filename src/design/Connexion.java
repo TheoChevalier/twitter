1e037203f22696ebf6f65b46e7905a0cc9225d97
LeaMiage <lea.cheoux@gmail.com>
2015-11-18 14:06:36 +0100

    ihm connexion

---
+++ src/design/Connexion.java
@@ -0,0 +1,79 @@
+package design;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+
+public class Connexion extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField tbxLogin;
+	private JPasswordField tbxMdp;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Connexion frame = new Connexion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Connexion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLogin = new JLabel("Login:");
+		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblLogin.setBounds(41, 95, 56, 16);
+		contentPane.add(lblLogin);
+		
+		JLabel lblMdp = new JLabel("Password:");
+		lblMdp.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblMdp.setBounds(41, 138, 99, 16);
+		contentPane.add(lblMdp);
+		
+		JLabel lblConnexion = new JLabel("Sign in");
+		lblConnexion.setHorizontalAlignment(SwingConstants.CENTER);
+		lblConnexion.setFont(new Font("Tahoma", Font.BOLD, 22));
+		lblConnexion.setBounds(12, 13, 408, 27);
+		contentPane.add(lblConnexion);
+		
+		tbxLogin = new JTextField();
+		tbxLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tbxLogin.setBounds(164, 88, 168, 22);
+		contentPane.add(tbxLogin);
+		tbxLogin.setColumns(10);
+		
+		JButton btnConnexion = new JButton("Sign in");
+		btnConnexion.setBounds(168, 192, 97, 25);
+		contentPane.add(btnConnexion);
+		
+		tbxMdp = new JPasswordField();
+		tbxMdp.setBounds(164, 132, 168, 22);
+		contentPane.add(tbxMdp);
+	}
+}
